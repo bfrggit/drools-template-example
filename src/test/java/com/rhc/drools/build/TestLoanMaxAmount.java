b9f72bcc78595550b16da91e0442b6d20b2666a2
Charles Zhu <zhuch@vmware.com>
2019-10-04 18:10:37 -0700

    Refined logger for the test class

--- src/test/java/com/rhc/drools/build/TestLoanMaxAmount.java
+++ src/test/java/com/rhc/drools/build/TestLoanMaxAmount.java
@@ -2,22 +2,29 @@ package com.rhc.drools.build;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.rhc.drools.model.Loan;
 
 public class TestLoanMaxAmount {
+   private static final Logger logger = LoggerFactory.getLogger(TestLoanMaxAmount.class);
+
    private static final String KIE_BASE = "loanKieBase";
    private KieContainer kieContainer;
    private KieBase kieBase;
 
    @BeforeEach
-   public void init() {
+   public void setUp() {
+      logger.warn("\n\n-------------------- setUp -----------------------\n");
+
       kieContainer = KieServices.Factory.get().getKieClasspathContainer();
       try {
          kieBase = kieContainer.getKieBase(KIE_BASE);
@@ -25,6 +32,13 @@ public class TestLoanMaxAmount {
          e.printStackTrace();
          throw e;
       }
+   }
+
+   @AfterEach
+   public void tearDown() {
+      logger.warn("\n\n-------------------- tearDown --------------------\n");
+
+      kieContainer.dispose();
    }
 
    @Test
@@ -50,7 +64,14 @@ public class TestLoanMaxAmount {
    public void testComplicatedLoan() {
       Loan loan = new Loan();
       loan.setIdentifier("complex");
-      KieSession kieSession = kieBase.newKieSession();
+      KieSession kieSession;
+
+      try {
+         kieSession = kieBase.newKieSession();
+      } catch (NullPointerException e) {
+         e.printStackTrace();
+         throw e;
+      }
 
       kieSession.insert(loan);
       kieSession.fireAllRules();
